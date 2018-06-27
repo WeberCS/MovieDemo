555dce0d108a2898d36ebd1e9d0933e204c09505
Richard Fry <Rich@richfry.com>
2018-06-27 07:47:55 -0600

    1st Version

---
+++ MovieDemo/Data/MovieDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieDemo.Models
+{
+    public class MovieDemoContext : DbContext
+    {
+        public MovieDemoContext (DbContextOptions<MovieDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieDemo.Models.Movie> Movie { get; set; }
+    }
+}
